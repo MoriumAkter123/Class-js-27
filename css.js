0a8f9b95ce5cf114dffdd4ac1a76ff0b1026460f
MoriumAkter123 <morium2312091068@diu.edu.bd>
2024-10-17 08:53:37 +0600

    class-27

---
+++ css.js
@@ -0,0 +1,20 @@
+let rahim = "car";
+let karim = "cycle";
+let rafic = "bike";
+
+if(rahim=="bike"){//true/false
+
+    console.log('I will go to Dinner');
+
+}
+ else if (karim=="car"){
+    console.log ('i will go to Coxs Bazar');
+}
+
+else if ( rafic=="cycle"){
+    console.log('i will go to Field');
+}
+else{
+
+    console.log('i will go to sleep')
+}
